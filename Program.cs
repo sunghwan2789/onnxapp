116eb6deaa79afe750af3ea953dadaa018f0fdeb
Sunghwan Bang <sunghwan2789@gmail.com>
2025-03-09 10:58:09 +0900

    update onnxruntime

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Diagnostics;
+using System.Numerics.Tensors;
 using Microsoft.ML.OnnxRuntime;
-using Microsoft.ML.OnnxRuntime.Tensors;
 
 var sessionOptions = new SessionOptions
 {
@@ -20,19 +20,26 @@ sessionOptions.AppendExecutionProvider_CoreML(CoreMLFlags.COREML_FLAG_CREATE_MLP
 sessionOptions.AppendExecutionProvider_CPU();
 
 using var session = new InferenceSession("matmul.onnx", sessionOptions);
-
-var t = new DenseTensor<float>([1500, 1500]);
+#pragma warning disable SYSLIB5001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+var t = Tensor.CreateUninitialized<float>([1500, 1500]);
 for (var i = 0; i < 1500; i++)
 for (var j = 0; j < 1500; j++)
     t[i, j] = Random.Shared.NextSingle() / 2;
 
 for (var i = 0; i < 10; i++)
 {
-    var x = NamedOnnxValue.CreateFromTensor("X", t);
+    var x = OrtValue.CreateTensorValueFromSystemNumericsTensorObject(t);
     var s = TimeProvider.System.GetTimestamp();
-    using var outputs = session.Run([x]);
-    var y = outputs[0].AsTensor<float>();
+
+    using var runOptions = new RunOptions();
+    using var outputs = session.Run(
+        runOptions,
+        new Dictionary<string, OrtValue> { ["X"] = x },
+        session.OutputNames
+    );
+    var y = outputs[0].GetTensorDataAsTensorSpan<float>();
     Console.WriteLine(TimeProvider.System.GetElapsedTime(s).TotalMilliseconds + " ms");
     // t = y.ToDenseTensor();
 }
 Debugger.Break();
+#pragma warning restore SYSLIB5001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
